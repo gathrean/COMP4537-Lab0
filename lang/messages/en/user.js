61d2de33072445289ad7aed91195725e1eb78470
Gathrean Dela Cruz <gathrean@icloud.com>
2024-01-09 20:21:45 -0800

    Added constant message strings

---
+++ lang/messages/en/user.js
@@ -0,0 +1,5 @@
+export const excellentMemoryAlert = 'Excellent memory!';
+
+export const wrongOrderAlert = 'Wrong order!';
+
+export const invalidNumberAlert = 'Please enter a valid number between 3 and 7.';
